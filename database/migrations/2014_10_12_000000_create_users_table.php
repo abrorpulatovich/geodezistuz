8b84f5deb2078f25c8ce2c40f633baa49b687a81
shoxijahon <shoxijahonislomov@mail.ru>
2022-01-08 15:10:50 +0500

    users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,15 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('full_name');
-            $table->integer('region_id');
             $table->string('username');
             $table->string('password');
-            $table->string('phone_number', 20);
-            $table->integer('old');
-            $table->string('specialist')->nullable();
             $table->string('email')->unique();
-            $table->boolean('gender');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
