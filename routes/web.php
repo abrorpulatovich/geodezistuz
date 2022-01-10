da987c6880d2326f5b22e3f45c81d750969cea95
shoxijahon <shoxijahonislomov@mail.ru>
2022-01-10 10:44:54 +0500

    routes1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CitizenController;
+use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\InstrumentController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\ResourceController;
+use App\Http\Controllers\RezumeController;
+use App\Http\Controllers\VacancyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +29,41 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::resource('answers', AnswerController::class);
+    Route::resource('questions', QuestionController::class);
+    Route::resource('comments', CommentController::class);
+    
+
+
+// admin
+
+Route::group([
+    'middleware' => ['auth', 'role:admin'],
+], function() {
+    Route::resource('companies', CompanyController::class);
+    Route::resource('citizens', CitizenController::class);
+    Route::resource('instruments', InstrumentController::class);
+    Route::resource('news', NewsController::class);
+    Route::resource('resources', ResourceController::class);
+    Route::resource('resumes', RezumeController::class);
+});
+
+// company
+
+Route::group([
+    'middleware' => ['auth', 'role:company'],
+], function() {
+    Route::resource('companies', CompanyController::class);
+    Route::resource('resumes', RezumeController::class);
+});
+
+//citizen
+
+Route::group([
+    'middleware' => ['auth', 'role:citizen'],
+], function() {
+    Route::resource('citizens', CitizenController::class);
+    Route::resource('vacancies', VacancyController::class);
+});
